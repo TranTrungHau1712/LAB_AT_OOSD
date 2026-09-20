0ee08e1c14fa235dfa94d7530dd29181a40cea13
TranTrungHau1712 <1250080050@sv.hcmunre.edu.vn>
2026-09-20 10:01:02 +0700

    LAB2: hoan thanh FrmMain va dang lam FrmDanhMuc

---
+++ LAB2/QuanLyThuVien/QuanLyThuMuc/Forms/FrmMain.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace QuanLyThuMuc.Forms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDanhMuc = new System.Windows.Forms.Button();
+            this.btnSach = new System.Windows.Forms.Button();
+            this.btnDocGia = new System.Windows.Forms.Button();
+            this.btnMuonTra = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDanhMuc
+            // 
+            this.btnDanhMuc.Location = new System.Drawing.Point(41, 100);
+            this.btnDanhMuc.Name = "btnDanhMuc";
+            this.btnDanhMuc.Size = new System.Drawing.Size(240, 60);
+            this.btnDanhMuc.TabIndex = 1;
+            this.btnDanhMuc.Text = "Danh mục / Nhân viên";
+            this.btnDanhMuc.UseVisualStyleBackColor = true;
+            this.btnDanhMuc.Click += new System.EventHandler(this.btnDanhMuc_Click);
+            // 
+            // btnSach
+            // 
+            this.btnSach.Location = new System.Drawing.Point(41, 227);
+            this.btnSach.Name = "btnSach";
+            this.btnSach.Size = new System.Drawing.Size(240, 60);
+            this.btnSach.TabIndex = 2;
+            this.btnSach.Text = "Quản lý đầu sách";
+            this.btnSach.UseVisualStyleBackColor = true;
+            this.btnSach.Click += new System.EventHandler(this.btnSach_Click);
+            // 
+            // btnDocGia
+            // 
+            this.btnDocGia.Location = new System.Drawing.Point(41, 367);
+            this.btnDocGia.Name = "btnDocGia";
+            this.btnDocGia.Size = new System.Drawing.Size(240, 60);
+            this.btnDocGia.TabIndex = 3;
+            this.btnDocGia.Text = "Độc giả và thẻ";
+            this.btnDocGia.UseVisualStyleBackColor = true;
+            this.btnDocGia.Click += new System.EventHandler(this.btnDocGia_Click);
+            // 
+            // btnMuonTra
+            // 
+            this.btnMuonTra.Location = new System.Drawing.Point(568, 100);
+            this.btnMuonTra.Name = "btnMuonTra";
+            this.btnMuonTra.Size = new System.Drawing.Size(240, 60);
+            this.btnMuonTra.TabIndex = 4;
+            this.btnMuonTra.Text = "Mượn - Trả sách";
+            this.btnMuonTra.UseVisualStyleBackColor = true;
+            this.btnMuonTra.Click += new System.EventHandler(this.btnMuonTra_Click);
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(568, 227);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(240, 60);
+            this.btnThongKe.TabIndex = 5;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(568, 367);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(240, 60);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTitle.Location = new System.Drawing.Point(198, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(522, 46);
+            this.lblTitle.TabIndex = 7;
+            this.lblTitle.Text = "HỆ THỐNG QUẢN LÝ THƯ VIỆN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 579);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.btnMuonTra);
+            this.Controls.Add(this.btnDocGia);
+            this.Controls.Add(this.btnSach);
+            this.Controls.Add(this.btnDanhMuc);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý thư viện";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDanhMuc;
+        private System.Windows.Forms.Button btnSach;
+        private System.Windows.Forms.Button btnDocGia;
+        private System.Windows.Forms.Button btnMuonTra;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
---
+++ LAB2/QuanLyThuVien/QuanLyThuMuc/Forms/FrmMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyThuMuc.Forms
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDanhMuc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSach_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDocGia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMuonTra_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
